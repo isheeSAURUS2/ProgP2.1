9bbed1a32b41656eb8ae064a43da1539e2032a1f
julien oving <37832@ma-web.nl>
2024-09-13 12:53:20 +0200

    opdrachten 1-3

--- prog/Assets/Script/BallSpawner.cs
+++ prog/Assets/Script/BallSpawner.cs
@@ -8,24 +8,55 @@ public class BallSpawner : MonoBehaviour
     public GameObject ball;
     public float elapsedTime;
     public Material BallMaterial;
+    
     // Start is called before the first frame update
-    void Start()
-    {
 
+    private void Start()
+    {
+        for (int i = 0; i < 100; i++)
+        {
+            float r = Random.Range(0f, 1f);
+            float g = Random.Range(0f, 1f);
+            float b = Random.Range(0f, 1f);
+            float x = Random.Range(-8.75f, 8.75f);
+            float y = Random.Range(0f, 5.75f);
+            float z = Random.Range(-4f, 4f);
+            Vector3 RandPos = new Vector3(x, y, z);
+            Color RandColor = new Color(r, g, b, 1f);
+            CreateBall(RandColor, RandPos);
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
+        float r = Random.Range(0f, 1f);
+        float g = Random.Range(0f, 1f);
+        float b = Random.Range(0f, 1f);
+        Color RandColor = new Color(r, g, b, 1f);
+        float x = Random.Range(-8.75f, 8.75f);
+        float y = Random.Range(-5.75f, 5.75f);
+        float z = Random.Range(-4f, 4f);
+        Vector3 RandPos = new Vector3(x, y, z);
         elapsedTime += Time.deltaTime;
         if(elapsedTime >= 1)
         {
             elapsedTime = 0;
-            CreateBall();
+            CreateBall(RandColor,RandPos);
         }
     }
-    private void CreateBall()
+    private void CreateBall(Color c , Vector3 Pos)
     {
-        Instantiate(ball);
+        GameObject CurrentBall = Instantiate(ball, Pos, Quaternion.identity);
+        BallMaterial = CurrentBall.GetComponent<MeshRenderer>().material;
+        if(elapsedTime >= 1)
+        {
+            Destroy(CurrentBall);
+        }
+
+        if (BallMaterial.shader.name == "Universal Render Pipeline/Lit")
+        {
+            BallMaterial.SetColor("_BaseColor", c);
+        }
     }
 }
--- prog/Assets/Script/Enemy.cs
+++ prog/Assets/Script/Enemy.cs
@@ -5,12 +5,12 @@ using UnityEngine;
 public class Enemy : MonoBehaviour
 {
     Vector3 velocity = new Vector3 (0f,0.5f,0f);
+    public Rigidbody rb;
 
 
     // Update is called once per frame
     void Update()
     {
-        transform.position = velocity;
-        velocity.z += 4 * Time.deltaTime;
+        rb.AddForce(0,0,4);
     }
 }
